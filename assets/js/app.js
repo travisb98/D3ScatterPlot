f5eadd3650edb4ba7c899f4ed8b4c47228a3229d
unknown <travisbostick@gmail.com>
2021-01-21 16:34:58 -0600

    working on the axis

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,6 +19,11 @@ var chartHeight = svgHeight - margin.top - margin.bottom;
 var chartWidth = svgWidth - margin.left - margin.right;
 
 
+/// initial y and x axis
+
+var chosenXAxis = "poverty";
+
+var chsenYAxis = "obesity";
 
 // create svg wrapper, append svg and set the width and height
 var svg=d3
@@ -33,17 +38,49 @@ var svg=d3
 var graphGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-/// function for updateing x scale variable
+
+
+
+/// function for updating x scale variable
 function updateXScale(xData,curXAxis){
     var xLineraScale = d3.scaleLinear()
         .domain(d3.min(xData, d=> d[curXAxis]),d3.max(xData, d=> d[curXAxis]))
-        .range([0,width]);
-
+        .range([0,chartWidth]);
     return xLineraScale;      
 
 }
 
-updateXScale()
+/// fucntion for updating y scale variable
+function updateYScale(yData,curYaxis){
+    var yLinearScale = d3.scaleLinear()
+        .domain(d3.min(yData, d=> d[curYaxis]),d3.max(yData, d=> d[curYaxis]))
+        .range([0,chartWidth]);  
+    return yLinearScale;
+}
+
+
+
+
+// function for updating xAxis when click
+function makeXAx(xScale,xAxis){
+    var bottomAx = d3.axisBottom(xScale);
+    xAxis.transition()
+        .duration(1000)
+        .call(bottomAx);
+    return xAxis;
+}
+
+// function for updating the y axis
+function makeYAx(yScale,yAxis){
+    var leftAx = d3.axisLeft(yScale);
+        yAxis.transition()
+        .duration(1000)
+        .call(leftAx);
+    return yAxis;
+}
+
+
+
 
 
 
@@ -52,8 +89,45 @@ d3.csv("assets/data/data.csv").then(function(data){
 
     /// for now we're just opening the data and console logging it
     // console.log(data);
+    // console.log(data.find(x => x.abbr == "AL"));
 
-    console.log(data.find(x => x.abbr == "AL"));
+
+
+    /// parse data, turning it into float
+    data.forEach(function(rowdata){
+
+        rowdata.poverty = parseFloat(rowdata.poverty);
+        rowdata.age=parseFloat(rowdata.age);
+        rowdata.income=parseFloat(rowdata.income);
+        rowdata.healthcare=parseFloat(rowdata.healthcare);
+        rowdata.obesity=parseFloat(rowdata.obesity);
+        rowdata.smokes=parseFloat(rowdata.smokes);
+    });
+
+
+
+    //// use the function updateXScale function to define xLinearScale
+    var xLinearScale=updateXScale(data,chosenXAxis);
+
+    var yLinearScale=updateYScale(data,chosenyAxis);
+
+
+
+
+
+
+
+    // // append x axis
+    // var xAxis = graphGroup.append("g")
+    // .classed("x-axis", true)
+    // .attr("transform", `translate(0, ${chartHeight})`)
+    // .call(bottomAxis);
+
+
+    // var chosenXAxis = "age";
+
+    // var xLinearScale=updateXScale(data,chosenXAxis);
+
 
 
 
